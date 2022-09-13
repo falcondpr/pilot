f1e465804dacebf83d74a9b697d8d725c795e2c5
fervillalbag <21fervillalbag@gmail.com>
2022-09-12 22:59:01 -0400

    feat: fetch info with react-query

--- apps/web/pages/admin/usuarios/[id].tsx
+++ apps/web/pages/admin/usuarios/[id].tsx
@@ -1,19 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { NextPage } from 'next';
-import { Box } from '@chakra-ui/react';
+import { useRouter } from 'next/router';
+import { useQuery } from '@tanstack/react-query';
+import { Box, Text } from '@chakra-ui/react';
 import { Input } from '@pilot/admin-ui';
 
 import Layout from '../../../components/admin/Layout';
+import { getUser } from '../../../utils/users';
+import { UserProps } from '../../../interfaces/user';
 
 const UsuarioAdmin: NextPage = () => {
+  const router = useRouter();
+  const [user, setUser] = useState<UserProps | null>(null);
+
+  const { data, isSuccess } = useQuery(['user', router?.query.id], () =>
+    getUser(router?.query.id as string)
+  );
+
+  useEffect(() => {
+    setUser(data?.data);
+  }, [isSuccess, data]);
+
   return (
     <Layout>
       <Box mb="20px">
-        <Input label="Nombre" />
+        <Input label="Nombre" value={user?.name} />
       </Box>
 
       <Box>
-        <Input label="Apellido" />
+        <Input label="Email" value={user?.email} />
+      </Box>
+
+      <Box mt="20px">
+        <Text>Description</Text>
       </Box>
     </Layout>
   );
